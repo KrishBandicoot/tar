22117caa717c820b0ee7aa5bd003be140c301819
KrishBandicoot <cristian.lizama.diaz2014@gmail.com>
2025-10-24 16:30:53 -0300

    Delete backend-kkarhua/fullrest directory

--- backend-kkarhua/fullrest/src/main/java/com/example/kkarhua/fullrest/repositories/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kkarhua.fullrest.repositories;
-import org.springframework.data.repository.CrudRepository;
-import com.example.kkarhua.fullrest.entities.Producto;
-
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
-
-}
