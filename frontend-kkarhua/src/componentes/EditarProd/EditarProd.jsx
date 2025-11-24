b155236605ce7759ca47d9d5480229a08260a2af
KrishBandicoot <cristian.lizama.diaz2014@gmail.com>
2025-11-23 23:45:14 -0300

    Delete frontend-kkarhua directory

--- frontend-kkarhua/src/componentes/EditarProd/EditarProd.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { Navbar } from '../Navbar/Navbar';
-
-export function EditarProd() {
-     
-}
