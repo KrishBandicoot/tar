1daadbc9df80e41e093478c1898df8120b81c171
KrishBandicoot <cristian.lizama.diaz2014@gmail.com>
2025-11-23 23:45:30 -0300

    Delete backend-kkarhua directory

--- backend-kkarhua/fullrest/src/main/java/com/example/kkarhua/fullrest/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kkarhua.fullrest.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.kkarhua.fullrest.entities.Categoria;
-
-public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
-    
-    boolean existsByNombre(String nombre);
-}
